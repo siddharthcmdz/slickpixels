98ee0ac364b3ce2e5d116c18e1391871e9a7f227
Siddharth Palaniappan <siddharth.cmdz@gmail.com>
2023-03-21 17:17:05 -0700

    add awesomefonts react for social media icons

--- src/components/SpFooter.js
+++ src/components/SpFooter.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faYoutube, faTwitter, faInstagram } from "@fortawesome/free-brands-svg-icons"
 
 const SpFooter = () => {
     return (
         <div className="spfooter">
-            <h1>Footer</h1>
+            <a href="https://www.youtube.com/channel/UCRCdAP4boepyS5Y7i3uKY1Q"
+                className="youtube social">
+                <FontAwesomeIcon icon={faYoutube} size="2x" />
+            </a>
+
+            <a href="https://twitter.com/siddharthcmdz"
+                className="twitter social">
+                <FontAwesomeIcon icon={faTwitter} size="2x" />
+            </a>
+
+            <a href="https://www.instagram.com/siddharth.cmdz/"
+                className="instagram social">
+                <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </a>
         </div>
     )
 }
