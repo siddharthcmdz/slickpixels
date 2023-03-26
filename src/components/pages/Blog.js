0fb19ed14d18986bc5f4d0f27e5089b1c12f5610
siddharthcmdz <siddharth.cmdz@gmail.com>
2023-03-25 17:53:12 -0700

    refined first blog

--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -7,12 +7,14 @@ const Blog = () => {
             <h1>My blogs</h1>
             <p>
                 Hello blogging world!
-                <br />
+                <br /><br />
                 This is my first blog. I created this website using reactjs and a plethora of other js libraries. I've hosted this site on google cloud through gcp app engine and its pretty interesting how seamless gcp works for hosting simple websites. I first designed this website on figma which can be viewed <a href="https://www.figma.com/file/F7G9cXPABDG57ljwfqIko4/SlickPixels.com?node-id=1%3A2&t=BIkQv2z36M33iP35-1" target="_blank" rel="noopener noreferrer">here</a>.
                 <br />
                 My subsequent project is to create a gltfdebugger which allows users to pick apart a gltf model and be able to modify its properties to visualize the different results. This will be using threeJS to begin with but may later on use a homegrown renderer!
+                <br /><br />
+                Stay tuned!  
                 <br />
-                Stay tuned!   
+                03-25-2023 
             </p>
         </div>
     )
