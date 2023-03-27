17363fa54efea22ab8cf61867a2e4245ea0b57f8
Siddharth Palaniappan <siddharth.cmdz@gmail.com>
2023-03-27 12:12:16 -0700

    minor refactors

--- src/components/pages/ContactMe.js
+++ src/components/pages/ContactMe.js
@@ -2,15 +2,6 @@ import React from 'react'
 import ContactForm from './ContactForm'
 import './pagestyles.css'
 
-const getContactForm = () => {
-   return (
-    <div className="contactform">
-        
-    </div>
-   ) 
-}
-
-
 const ContactMe = () => {
     return (
         <div className="contactform">
